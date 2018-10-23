344942a8f0fd1f0ffa3f959bd44662bdec30797e
Spillz <billygcasey@hotmail.com>
2018-10-23 15:43:25 -0500

    Working on an add inventory

--- src/main/java/org/launchcode/general_Store/controllers/InventoryController.java
+++ src/main/java/org/launchcode/general_Store/controllers/InventoryController.java
@@ -3,7 +3,11 @@ package org.launchcode.general_Store.controllers;
 import org.launchcode.general_Store.models.Inventory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "general_Store/Inventory")
@@ -25,6 +29,16 @@ public class InventoryController {
         return "general_Store/Inventory/add";
     }
 
-
+//    @RequestMapping(value = "add")
+//    public String processAddInventory(Model model, Errors errors, @ModelAttribute @Valid Inventory newInvItem) {
+//        if (errors.hasErrors()) {
+//            model.addAttribute("title", "Create inventory item");
+//            return "general_Store/Inventory/add";
+//        }
+//
+//        model.addAttribute("title", "Create inventory item");
+//        System.out.println("InventoryController-addpro");
+//        return "general_Store/Inventory";
+//    }
 
 }

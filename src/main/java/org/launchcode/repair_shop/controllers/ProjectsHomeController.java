1c91af81ed5bf53d6aca978a8ed62d71393b40dc
Spillz <billygcasey@hotmail.com>
2019-08-09 14:32:51 -0500

    Fixing up view

---
+++ src/main/java/org/launchcode/repair_shop/controllers/ProjectsHomeController.java
@@ -0,0 +1,18 @@
+package org.launchcode.repair_shop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class ProjectsHomeController {
+    //Displays the Home page of the entire app
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "Select an App");
+        System.out.println("ProjectsHomeController");
+        return "projectHome";
+    }
+
+}
